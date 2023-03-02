0a6f8ee15866adc6d7ce677629c338660ecdfede
Rohan Maharjan <smdnoobs24@gmail.com>
2023-03-02 15:44:34 +0545

    added padding

--- src/components/Navbar/component.tsx
+++ src/components/Navbar/component.tsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center bg-white dark:bg-dark-blue-400">
+    <nav className="flex justify-between items-center bg-white dark:bg-dark-blue-400 px-8">
       <div className="">Where in the world?</div>
       <div className="">Dark Mode</div>
     </nav>
